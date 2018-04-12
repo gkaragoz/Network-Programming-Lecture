bef623766ee7ca692513a81054f6d5a62af5b79a
gkaragoz <annotto3@gmail.com>
2018-04-12 14:48:13 +0300

    Require body-parser

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const bodyParser= require('body-parser')
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.listen(3000, function() {
     console.log('listening on 3000')
 })
 
+// app.post('/exampleID', (req, res) => {
+//     console.log(req.body)
+// })
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
-    // Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get!
-    // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
 })
